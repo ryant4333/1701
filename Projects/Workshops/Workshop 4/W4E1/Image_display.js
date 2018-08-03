18f336a603af3cbd88dce6d85f02e2107400c664
ryant4333 <RyanCTaylor95@gmail.com>
2018-08-03 12:21:52 +1000

    8-3-188 12PM

--- Projects/Workshops/Workshop 4/W4E1/Image_display.js
+++ Projects/Workshops/Workshop 4/W4E1/Image_display.js
@@ -1,8 +1,9 @@
 var img;
+var img2;
 
 function preload() {
   img = loadImage("https://vignette.wikia.nocookie.net/pokemonfakemon/images/1/17/Poliwhirl_Dream.png/revision/latest?cb=20110910231534");
-  img2 = loadImage("Images/Poliwhirl.png");
+  //img2 = loadImage("Images/Poliwhirl.png");
 }
 function setup() {
   createCanvas(600, 600);
@@ -10,5 +11,5 @@ function setup() {
 
 function draw() {
   image(img, 100, 100);
-  image(img2, 200, 200);
+  //image(img2, 200, 200);
 }
